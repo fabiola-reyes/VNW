a5358eca8b4de270a715582530a708b807a9efdc
fabiola reyes <reyesfabiola2019@gmail.com>
2025-04-03 14:01:26 -0300

    enlace

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -2,8 +2,27 @@
        
         import S from './LivrosDoados.module.scss'
         import livro from '../../assets/livro.png'
+        import axios from 'axios'
+        import {useState, useEffect} from 'react' 
         
         export default function Doados(){
+
+            const [livros, setLivros] = useState([])
+
+         
+            const getLivros = async () => {
+                try {
+                    const response = await axios.get("https://teste-deploy-bkai.onrender.com/livros")
+                    setLivros(response.data)
+                } catch (error) {
+                    console.error("Erro ao buscar livros:", error)
+                }
+            }
+        
+        
+            useEffect(() => {
+                getLivros()
+            }, [])
             return(
                 <section className={S.boxDoados}>
                     <h2>Livros Doados</h2>
--- src/pages/querodoar/QueroDoar.jsx
+++ src/pages/querodoar/QueroDoar.jsx
@@ -1,11 +1,61 @@
 import S from './QueroDoar.module.scss'
 import vector from '../../assets/vector.png'
+import axios from 'axios'
+import {useState, useEffect} from 'react' 
 
 export default function QueroDoar(){
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+    const enviarDados = async()=>{
+        
+        const urlApi https://flask-y9s7.onrender.com
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+
+        await axios.post(urlApi, dadosEnviar)
+
+        alert("Livro cadastrado!!")
+
+        
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+    }
+
+    
+    const capturaTitulo = (e) =>{
+        setTitulo(e.target.value)
+        console.log(e.target.value) 
+    }
+
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+
+    const capturaImagem = (e) =>{
+        setImagem_url(e.target.value)
+    }
+
     return(
         <section className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+
+                <form onSubmit={(e)=> e.preventDefault()}>
                 <form action="">
                   <div>
                     <img src={vector} alt="icone de um livro" />
