e99e2ce0483a9446f40f0d19c20373fed2cb9d62
fabiola reyes <reyesfabiola2019@gmail.com>
2025-04-04 10:27:39 -0300

    pronto

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -2,7 +2,6 @@
        
         import S from './LivrosDoados.module.scss'
         import livro from '../../assets/livro.png'
-        import harry from '../../assets/harry.jpg'
         import axios from 'axios'
         import {useState, useEffect} from 'react' 
         
@@ -13,7 +12,7 @@
          
             const getLivros = async () => {
                 try {
-                    const response = await axios.get("https://teste-deploy-bkai.onrender.com/livros")
+                    const response = await axios.get("https://flask-y9s7.onrender.com/livros")
                     setLivros(response.data)
                 } catch (error) {
                     console.error("Erro ao buscar livros:", error)
@@ -28,32 +27,16 @@
                 <section className={S.boxDoados}>
                     <h2>Livros Doados</h2>
                     <section className={S.boxcard}>
-                        <article>
-                            <img src={livro} alt="imagem de um livro que se chama o protagonista" />
-                            
-                            <h3>O Protagonista</h3>
-                            <p>Susanne Andrade</p>
-                            <p>Ficção</p>
-                            
-                        </article>
-
-                        <article>
-                            <img src={livro} alt="imagem de um livro que se chama o protagonista" />
-                            
-                            <h3>A Arte da Guerra</h3>
-                            <p>Sun Tzu</p>
-                            <p>NaoFicção</p>
-                            
-                        </article>
-
-                        <article>
-                            <img src={harry} alt="imagem de um livro que se chama o protagonista" />
-                            
-                            <h3>Harry Potter e a Pedra filosofal</h3>
-                            <p>J.K Rowling</p>
-                            <p>Ficção</p>
-                            
-                        </article>
+                    {livros.map((item) => (
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt={item.titulo} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </article>
+                ))}
+            
+        
                     </section>
                 </section>
             )
